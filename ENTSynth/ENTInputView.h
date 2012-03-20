ec2d5d2860ae131855688c1ccb78653766887ef3
Rob O'Connor <roconnor@wit.ie>
2012-03-20 16:57:13 +0000

    Updating README

---
+++ ENTSynth/ENTInputView.h
@@ -0,0 +1,13 @@
+//
+//  ENTInputView.h
+//  ENTSynth
+//
+//  Created by Robert O'Connor on 16/03/2012.
+//  Copyright (c) 2012 WIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ENTInputView : UIView
+
+@end
---
+++ ENTSynth/ENTInputView.m
@@ -0,0 +1,31 @@
+//
+//  ENTInputView.m
+//  ENTSynth
+//
+//  Created by Robert O'Connor on 16/03/2012.
+//  Copyright (c) 2012 WIT. All rights reserved.
+//
+
+#import "ENTInputView.h"
+
+@implementation ENTInputView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
